fa8a51d22eb87146c2e977cae3d770bee17346c2
Alonso Quinteros <quinteros.alonso@gmail.com>
2026-07-13 09:19:51 -0400

    Base de Funciones

--- examen.py
+++ examen.py
@@ -1,3 +1,12 @@
+# Datos
+'''
+"titulo"	    Nombre de la película	No debe contener solo espacios en blanco ni estar vacío
+"genero"    	Género cinematográfico	No debe contener solo espacios en blanco ni estar vacío
+"duracion_min"	Duración en minutos	Número entero mayor que cero
+"clasificacion" Clasificación de audiencia	Debe ser exactamente 'A', 'B' o 'C'
+"idioma"	    Idioma original de la película	No debe contener solo espacios en blanco ni estar vacío
+"es_3d"	        Indica si la función es en formato 3D	El usuario ingresa 's' o 'n'. El sistema lo convierte a True o False
+'''
 peliculas = {
     'P101': ['Luz de Otoño', 'drama', 110, 'B', 'Español', False],
     'P102': ['Noche Neón', 'acción', 125, 'C', 'Ingles', True],
@@ -7,6 +16,10 @@ peliculas = {
     'P106': ['Viaje Lunar', 'ciencia ficción', 132, 'B', 'Ingles', False],
 }
 
+'''
+"precio"    	Precio de la entrada en pesos	Número entero mayor que cero
+"cupos"     	Cantidad de cupos disponibles para esa función	Número entero mayor o igual a cero
+'''
 cartelera = {
     'P101': [5990, 40],
     'P102': [7990, 0],
@@ -15,7 +28,58 @@ cartelera = {
     'P105': [8990, 8],
     'P106': [7490, 3]
 }
+# Fin Datos
 
+# Opciones
+def cupos_genero(genero):
+    pass
+
+def busqueda_precio(p_min, p_max):
+    print()
+
+def actualizar_precio(codigo, nuevo_precio):
+    return True
+
+# Utilidades de crear
+'''
+código          No vacío ni solo espacios en blanco, y que no exista ya en los diccionarios
+título	        No vacío ni solo espacios en blanco
+género	        No vacío ni solo espacios en blanco
+duración	    Número entero mayor que cero
+clasificación	Debe ser exactamente 'A', 'B' o 'C'
+idioma  	    No vacío ni solo espacios en blanco
+es_3d   	    El usuario ingresa 's' o 'n'. El sistema almacena True si es 's', False si es 'n'
+precio  	    Número entero mayor que cero
+cupos       	Número entero mayor o igual a cero
+'''
+def rev_codigo():
+    pass
+def rev_titulo():
+    pass
+def rev_genero():
+    pass
+def rev_duracion():
+    pass
+def rev_clasificacion():
+    pass
+def rev_idioma():
+    pass
+def rev_es_3d():
+    pass
+def rev_precio():
+    pass
+def rev_cupos():
+    pass
+# Fin Utilidades de crear
+
+def agregar_pelicula(codigo, titulo, genero, duracion, clasificacion, idioma, es_3d, precio, cupos):
+    return True
+
+def eliminar_pelicula(codigo):
+    return True
+# Fin Opciones
+
+# Funcion Principal
 
 def menu():
     while True:
@@ -46,3 +110,7 @@ def menu():
                     pass
         except:
             pass
+# Fin Funcion Principal
+
+# Ejecutar
+menu()
